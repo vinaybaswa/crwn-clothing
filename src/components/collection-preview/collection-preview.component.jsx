5363731c25dd186a4e40cc1dbf285d61e6a008d7
vinay baswa <vinaybaswa@gmail.com>
2021-05-18 13:08:14 -0400

    styling shop page, collection preview component and collection item

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import '../collection-item/collection-item.component'
+import CollectionItem from '../collection-item/collection-item.component';
+
 import './collection-preview.styles.scss'
 
 const CollectionPreview = ({ title, items }) => (
@@ -8,11 +11,10 @@ const CollectionPreview = ({ title, items }) => (
     <div className='preview' >
       {items
         .filter((item, idx) => idx < 4)
-        .map(item => (
-          <div key={item.id}> {item.name} </div>
+        .map(({ id, ...otherItemProps }) => (
+          <CollectionItem key={id} {...otherItemProps} />
         ))}
     </div>
-
   </div>
 );
 
