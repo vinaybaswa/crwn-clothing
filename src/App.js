2771dcdffffa54fbbe5e1f400b62d12d0feb385a
vinay baswa <vinaybaswa@gmail.com>
2024-09-23 06:50:45 -0400

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="categories-container">
+      <div className="category-container">
+        <div className="category-body-container">
+          <h1>Hats</h1>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        <div className="category-body-container">
+          <h1>Jackets</h1>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        <div className="category-body-container">
+          <h1>Sneakers</h1>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        <div className="category-body-container">
+          <h1>Womens</h1>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        <div className="category-body-container">
+          <h1>Mens</h1>
+          <p>Shop Now</p>
+        </div>
+      </div>
     </div>
   );
 }
