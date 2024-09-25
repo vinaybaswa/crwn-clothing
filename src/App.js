fc78a0cad75aa3fec3842fa036486cf699184722
vinay baswa <vinaybaswa@gmail.com>
2024-09-24 22:14:52 -0400

    use Routes, Route, Outlet and Link from reactRouterDom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import Directory from './components/directory/directory.component';
+import { Routes, Route } from 'react-router-dom';
+import Navigation from './routes/navigation/navigation.component';
+import Home from './routes/home/home.component';
 
 const App = () => {
   return (
-    <Directory/>
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+      </Route>
+    </Routes>
   );
 }
 
