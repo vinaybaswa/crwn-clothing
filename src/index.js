fe1d6203fa30a0b4e7a661e13e634062557bff58
vinay baswa <vinaybaswa@gmail.com>
2024-10-21 16:12:35 -0400

    adding cart context, shopping icon and cart dropdown

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 import App from "./App";
 import { UserProvider } from "./contexts/user.context.jsx";
 import { ProductsProvider } from "./contexts/products.context.jsx";
+import { CartProvider } from "./contexts/cart.context.jsx";
 
 import "./index.scss";
 
@@ -15,7 +16,9 @@ root.render(
     <BrowserRouter>
       <UserProvider>
         <ProductsProvider>
-          <App />
+          <CartProvider>
+            <App />
+          </CartProvider>
         </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
