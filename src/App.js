62bc9aecb378fa0676a9acef4be2d6695c248b8e
vinay baswa <vinaybaswa@gmail.com>
2024-12-03 15:30:12 -0500

    implement checkUserSession saga

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Routes, Route } from "react-router-dom";
 
-import {
-  onAuthStateChangedListener,
-  createUserDocumentFromAuth,
-} from "./utils/firebase/firebase.utils.js";
-import { setCurrentUser } from "./store/user/user.action.js";
+import { checkUserSession } from "./store/user/user.action.js";
 import Navigation from "./routes/navigation/navigation.component";
 import Home from "./routes/home/home.component";
 import Authentication from "./routes/authentication/authentication.component";
@@ -17,16 +13,8 @@ const App = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChangedListener((user) => {
-      if (user) {
-        createUserDocumentFromAuth(user);
-      }
-
-      dispatch(setCurrentUser(user));
-    });
-
-    return unsubscribe;
-  }, [dispatch]);
+    dispatch(checkUserSession());
+  }, []);
 
   return (
     <Routes>
