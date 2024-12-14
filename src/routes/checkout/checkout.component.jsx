0afecd08bdb8ff5376f370d68c4f92a83c4ab105
vinay baswa <vinaybaswa@gmail.com>
2024-12-14 02:17:57 -0500

    write serverless functions and setup stripe payment

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,8 +1,12 @@
 import { useSelector } from "react-redux";
 
-import { selectCartItems, selectCartTotal } from "../../store/cart/cart.selector";
+import {
+  selectCartItems,
+  selectCartTotal,
+} from "../../store/cart/cart.selector";
 
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import PaymentForm from "../../components/payment-form/payment.form.component";
 
 import {
   CheckoutContainer,
@@ -34,10 +38,11 @@ const Checkout = () => {
           <span>Remove</span>
         </HeaderBlock>
       </CheckoutHeader>
-      {cartItems.map((item) => (
-        <CheckoutItem key={item.id} cartItem={item} />
+      {cartItems.map((cartItem) => (
+        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
       ))}
       <Total>Total: ${cartTotal}</Total>
+      <PaymentForm />
     </CheckoutContainer>
   );
 };
