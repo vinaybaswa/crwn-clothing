7b2e01f5b963fa1b1b83443789d53908b801508c
vinay baswa <vinaybaswa@gmail.com>
2025-01-10 21:22:36 -0500

    type sign-in and sign-up components

--- src/components/sign-in-form/sign-in-form.component.tsx
+++ src/components/sign-in-form/sign-in-form.component.tsx
@@ -1,5 +1,7 @@
-import { useState } from "react";
+import { useState, FormEvent, ChangeEvent } from "react";
 import { useDispatch } from "react-redux";
+
+import { AuthError, AuthErrorCodes } from "firebase/auth";
 
 import FormInput from "../form-input/form-input.component";
 import Button, { BUTTON_TYPE_CLASSES } from "../button/button.component";
@@ -28,15 +30,16 @@ const SignInForm = () => {
     dispatch(googleSignInStart());
   };
 
-  const handleSubmit = async (event) => {
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     try {
       dispatch(emailSignInStart(email, password));
-
       resetFormFields();
     } catch (error) {
-      if (error.code === "auth/invalid-credential") {
+      if (
+        (error as AuthError).code === AuthErrorCodes.INVALID_LOGIN_CREDENTIALS
+      ) {
         alert("Invalid cedentials");
       } else {
         console.log(error);
@@ -44,7 +47,7 @@ const SignInForm = () => {
     }
   };
 
-  const handleChange = (event) => {
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
     setFormFields({ ...formFields, [name]: value });
   };
